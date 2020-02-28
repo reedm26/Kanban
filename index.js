bea3fce3b8d2c2d7b3c4af83e32780b6b92d8946
reedm26 <reedm26@yahoo.com>
2020-02-27 21:23:01 -0700

    first few things

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-require('dotenv').config()
-require = require("esm")(module)
-module.exports = require("./server/main")
+require(dotenv).config({ path: "../.env" });
+require = require("esm")(module);
+module.exports = require("./server/main");
