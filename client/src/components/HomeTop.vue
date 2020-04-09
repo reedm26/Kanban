aeb56f1407d20968aa6a704b8b5e52d63e2095e8
reedm26 <reedm26@yahoo.com>
2020-04-08 23:38:11 -0600

    added sweet alert

--- client/src/components/HomeTop.vue
+++ client/src/components/HomeTop.vue
@@ -31,6 +31,23 @@ export default {
       this.$store.dispatch("addBoard", this.newBoard);
       this.newBoard = { title: "", description: "" };
     },
+    const{ value: formValues } = await Swal.fire({
+  title: 'Multiple inputs',
+  html:
+    '<input id="swal-input1" class="swal2-input">' +
+    '<input id="swal-input2" class="swal2-input">',
+  focusConfirm: false,
+  preConfirm: () => {
+    return [
+      document.getElementById('swal-input1').value,
+      document.getElementById('swal-input2').value
+    ]
+  }
+})
+
+if (formValues) {
+  Swal.fire(JSON.stringify(formValues))
+}
   },
 };
 </script>
